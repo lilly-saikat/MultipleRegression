41119e8e893bb3a0ab51c8a57499cdab732dd00b
lilly-saikat <choudhury_saikat@lilly.com>
2017-09-04 07:35:01 +0530

    Earth Model Error

--- src/models/basic_models.py
+++ src/models/basic_models.py
@@ -12,11 +12,11 @@ import os
 # For Linear model
 import statsmodels.api as sm
 import pickle
-from pyearth import Earth
+# from pyearth import Earth
 import matplotlib.pyplot as plt
 import numpy as np
 # =============================================================================
-#  Define Location
+#  Define Location;
 # =============================================================================
 path = os.getcwd()
 
@@ -49,15 +49,14 @@ def mars_comp_model(data, penalty, vibrose, dep_var, ind_var):
     model.fit(data[ind_var], data[dep_var])
     print(model.summary())
     data['Predicted'] = model.predict(data[dep_var])
-    fig = plt.figure(figsize=[20,10])
+    fig = plt.figure(figsize=[20, 10])
     ax1 = fig.add_subplot(121)
     ax2 = fig.add_subplot(122)
     ax1.scatter(data[ind_var], data['Predicted'])
     ax2.scatter(data[ind_var], data[dep_var])
-    plt.savefig(path+"/models/.jpg")
+    plt.savefig(path+"/models/py-earth.jpg")
     mape = np.mean(np.abs(data[dep_var]-data['Predicted'])/data[dep_var])
     print(mape)
-
 
 
 def mars_full_model(data, penalty, vibrose, drop_vars, dep_var):
